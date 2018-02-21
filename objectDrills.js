f381a55d8e929d64f2cbf29b65004e321432eae3
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-02-21 11:54:43 -0800

    added hobbit meal

--- objectDrills.js
+++ objectDrills.js
@@ -1,12 +1,34 @@
-const loaf = {
-    flour: 300,
-    water: 210,
-    hydration: function() {
-        let result = (loaf.water/loaf.flour) * 100;
-        return result;
-    }
-}
+// const loaf = {
+//     flour: 300,
+//     water: 210,
+//     hydration: function() {
+//         let result = (loaf.water/loaf.flour) * 100;
+//         return result;
+//     }
+// }
 
-console.log('flour:', loaf.flour);
-console.log('water:', loaf.water);
-console.log('hydration:', loaf.hydration());
+// console.log('flour:', loaf.flour);
+// console.log('water:', loaf.water);
+// console.log('hydration:', loaf.hydration());
+
+
+
+// const random = {
+//     foo: 2,
+//     bar: 'snickers',
+//     fum: 'red',
+//     quux: 'what',
+//     spam: 'delete'
+// };
+
+// for(let key in random) {
+//     console.log(key, random[key]);
+// }
+
+
+
+const food = {
+    meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+
+console.log(food.meals[3]);
